735a7d56ec693ed69672469391b83b80a306d30c
ankit-suman-07 <ankitsuman07@gmail.com>
2023-10-22 15:12:46 +0530

    Made navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import "./Navbar.css";
 
 import LogoIcon from "../../assets/logo.png";
 
 const Navbar = () => {
+    const [cross, setCross] = useState(false);
+
+    const crossBtn = () => {
+        setCross(!cross);
+    }
     return (
         <div className='navbar' >
             <div className='logo' >
@@ -22,6 +27,28 @@ const Navbar = () => {
                 <NavLink to="/contact" activeClassName="active" className="link" ><span>Contact Us</span></NavLink>
                 <NavLink to="/saved" activeClassName="active" className="link" ><span>Saved</span></NavLink>
                 <NavLink to="/room" activeClassName="active" className="link" ><span>My Room</span></NavLink>
+            </div>
+
+            <div className={cross ? "cross" : "hamburger-menu"} onClick={crossBtn} >
+                <div className='bar' ></div>
+                <div className='bar mid' ></div>
+                <div className='bar' ></div>
+            </div>
+
+            <div className={cross ? "show-menu" : "hide-menu"} >
+                <div className='logo-res' >
+                    <div className='logo-img' >
+                        <img src={LogoIcon} alt='logo' />
+                    </div>
+                    <div className='logo-text' >
+                        GameIt
+                    </div>
+                </div>
+                <NavLink exact to="/" activeClassName="active" className="link" onClick={crossBtn} ><span>Home</span></NavLink>
+                <NavLink to="/genres" activeClassName="active" className="link" onClick={crossBtn} ><span>Genres</span></NavLink>
+                <NavLink to="/contact" activeClassName="active" className="link" onClick={crossBtn} ><span>Contact Us</span></NavLink>
+                <NavLink to="/saved" activeClassName="active" className="link" onClick={crossBtn} ><span>Saved</span></NavLink>
+                <NavLink to="/room" activeClassName="active" className="link" onClick={crossBtn} ><span>My Room</span></NavLink>
             </div>
 
         </div>
