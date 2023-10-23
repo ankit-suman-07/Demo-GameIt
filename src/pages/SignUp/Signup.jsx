1e787578b3815ef79e18ac2a95aa0a4f9f0e889f
ankit-suman-07 <ankitsuman07@gmail.com>
2023-10-23 13:52:01 +0530

    Completed signup page

--- src/pages/SignUp/Signup.jsx
+++ src/pages/SignUp/Signup.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 import "./Signup.css";
 
 import BackGround from "../../assets/signup-bg.png";
+import GoogleIcon from "../../assets/google.png";
 
 const Signup = () => {
     return (
         <div className='signup' >
             <img src={BackGround} alt='background' />
+            <div className='signup-box-outer' >
+                <div className='signup-box' >
+                    <div className='signin-inner' >
+                        <input type='email' placeholder='Email ID' />
+                        <input type='password' placeholder='Password' />
+                        <button>Log In</button>
+                        <div className='mid-text' >
+                            Or, Sign In using Google
+                        </div>
+                        <img src={GoogleIcon} placeholder='google icon' />
+                    </div>
+                    <div className='signup-inner' >
+                        Not an existing user? Click Here
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
