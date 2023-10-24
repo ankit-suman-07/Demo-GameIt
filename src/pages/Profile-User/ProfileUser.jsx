fd4225dcc72d547e9ae724007830213ccece72f4
ankit-suman-07 <ankitsuman07@gmail.com>
2023-10-24 14:47:03 +0530

    Completed user's profile page

--- src/pages/Profile-User/ProfileUser.jsx
+++ src/pages/Profile-User/ProfileUser.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import "./ProfileUser.css";
 import Navbar from '../../components/navbar/Navbar';
+import ProfileCard from '../../components/profilecard/ProfileCard';
+import GameSlider from '../../components/games-slider/GameSlider';
 
 const ProfileUser = () => {
     return (
@@ -8,10 +11,21 @@ const ProfileUser = () => {
                 <Navbar />
             </nav>
             <section>
-
+                <ProfileCard />
             </section>
             <main>
-
+                <div className="slider-head" >
+                    Currently Playing
+                </div>
+                <GameSlider />
+                <div className="slider-head" >
+                    Saved Games
+                </div>
+                <GameSlider />
+                <div className="slider-head" >
+                    Games Owned
+                </div>
+                <GameSlider />
             </main>
         </div>
     )
